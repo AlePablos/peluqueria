35d76dbf8857fc17e1a84c9b1913fddcd547fe92
Federico Ramirez <federico_r@beezwax.net>
2015-09-22 22:12:46 -0300

    Agrega end_time a appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment < ActiveRecord::Base
   has_and_belongs_to_many :work_items
   belongs_to :user
+
+  def end_time
+    start_time + work_items.reduce(0) { |minutes, work_item| minutes + work_item.duration.minutes }
+  end
 end
