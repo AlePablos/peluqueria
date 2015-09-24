ba8a91e30290ba8f004ba63f91a255c61f98a456
Federico Ramirez <federico_r@beezwax.net>
2015-09-24 12:45:17 -0300

    Mejores vistas

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -1,2 +1,9 @@
 module AppointmentsHelper
+  def appointment_summary(appointment)
+    "#{appointment.user.full_name}: #{appointment_format_time(appointment.start_time)} - #{appointment_format_time(appointment.end_time)}"
+  end
+
+  def appointment_format_time(time)
+    time.strftime('%H:%M')
+  end
 end
