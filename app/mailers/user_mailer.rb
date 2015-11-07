1773f283b6619af558a2ae268c968d11e0f76498
AlePablos <alepablos03@gmail.com>
2015-11-07 20:27:43 -0300

    Disable mails for now

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
 class UserMailer < ApplicationMailer
   def confirmation_email(user)
-    @user = user
-    @url  = 'http://example.com/'
-    mail(to: @user.email, subject: 'Confirma tu cuenta')
+    true
+    #@user = user
+    #@url  = 'http://example.com/'
+    #mail(to: @user.email, subject: 'Confirma tu cuenta')
   end
 end
