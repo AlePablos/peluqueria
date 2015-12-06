554b6cdd7f4730c612369047eb8a68d690f5d62a
AlePablos <alepablos03@gmail.com>
2015-12-05 22:10:03 -0300

    Validar appointment

--- app/models/work_item.rb
+++ app/models/work_item.rb
@@ -1,3 +1,6 @@
 class WorkItem < ActiveRecord::Base
   has_and_belongs_to_many :appointments
+
+  validates :duration, numericality: { greater_than: 0, smaller_than: 12.hours.to_i }
+  validates :price,    numericality: { greater_than: 0 }
 end
